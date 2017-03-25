cb83c9967e1d506dcd30719e4b1397f8e51cbf60
eduzen <eduardo.a.enriquez@gmail.com>
2017-03-24 22:31:30 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from talleres import talleres
+
+for taller in talleres:
+    print(taller)
+    print(taller['total_inscriptos'])
+
+
+
+"""
+
+
+mayor_numero_de_inscriptos
+
+superposicion_de_horarios and
+horario_disponible and
+"""
---
+++ talleres.py
@@ -0,0 +1,41 @@
+taller1 = {
+    'responsable': 'Zoe',
+    'nombre': 'Mi taller',
+    'total_inscriptos': 30,
+    'tags': [
+        'charla', 'taller',
+    ],
+    'categoria': 'principiante',
+}
+
+taller2 = {
+    'responsable': 'Daniela',
+    'nombre': 'Otro taller',
+    'total_inscriptos': 20,
+    'tags': [
+        'charla', 'taller',
+    ],
+    'categoria': 'avanzado',
+}
+
+taller3 = {
+    'responsable': 'Cristina',
+    'nombre': 'Algun otro taller',
+    'total_inscriptos': 3,
+    'tags': [
+        'charla', 'taller',
+    ],
+    'categoria': 'principiante',
+}
+
+taller4 = {
+    'responsable': 'Luis',
+    'nombre': 'Otro tipo taller',
+    'total_inscriptos': 2,
+    'tags': [
+        'charla', 'taller',
+    ],
+    'categoria': 'intermedio',
+}
+
+talleres = [taller1, taller2, taller3, taller4]
