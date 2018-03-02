51aac3186a734cd0ac39159e3244ba274218f2bc
Winna_Z <zoem@infobytesec.com>
2018-03-02 15:19:14 -0300

    added documentation

--- imput_projects.py
+++ imput_projects.py
@@ -2,5 +2,6 @@
 import json
 from pprint import pprint
 
+#loads the data from an existing .json into a dictiorary
 data = json.load(open('input_example.json'))
-pprint(data)
+pprint(data['projects'])
