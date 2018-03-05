6f105896e8bcdf889bfdbc5fd034e7bc9c4ca399
Winna_Z <zoem@infobytesec.com>
2018-03-05 12:38:55 -0300

    pep8

--- imput_projects.py
+++ imput_projects.py
@@ -5,58 +5,58 @@ from pprint import pprint
 # loads the data from an existing .json into a dictiorary
 data = json.load(open('input_example.json'))
 
-UserName = str(input("¿Cuál es tu nombre?  "))
-ProjectName = str(input("Ingresá el Nombre del Proyecto a proponer  "))
+user_name = str(input("¿Cuál es tu nombre?  "))
+project_name = str(input("Ingresá el Nombre del Proyecto a proponer  "))
 
-data['projects'][ProjectName] = {}
-ProjectDif = 13
+data['projects'][project_name] = {}
+project_dif = 13
 
-while ProjectDif not in range(1, 11):
-    ProjectDif = input("Ingresá el grado de dificultad del proyecto (1-10)   ")
-    if ProjectDif.isnumeric():
-        ProjectDif = int(ProjectDif)
+while project_dif not in range(1, 11):
+    project_dif = input("Ingresá el grado de dificultad del proyecto (1-10)   ")
+    if project_dif.isnumeric():
+        project_dif = int(project_dif)
 
-data['projects'][ProjectName]['difficult_level'] = ProjectDif
-RespCheck = input("Sos el responsable de este proyecto? Si/No  ")
-RespCheck = RespCheck.lower()
+data['projects'][project_name]['difficult_level'] = project_dif
+resp_check = input("Sos el responsable de este proyecto? Si/No  ")
+resp_check = resp_check.lower()
 
 
-while RespCheck != "si" and RespCheck != "no":
-
-    print("Por favor responde con 'Si' o 'No'")
-    RespCheck = input("Sos el/la responsable de este proyecto? Si/No  ")
-    RespCheck = RespCheck.lower()
-
-if RespCheck == "si":
-
-    data['projects'][ProjectName]['responsables'] = [UserName, ]
-
-elif RespCheck == "no":
-
-    ProjectResp = input("Por favor ingresa el nombre del responsable  ")
-    data['projects'][ProjectName]['responsables'] = ProjectResp
-
-addresp = str(input("¿Hay otrx responsable del proyecto? Si/No  "))
-addresp = addresp.lower()
-
-while addresp != "si" and addresp != "no":
+while resp_check != "si" and resp_check != "no":
 
     print("Por favor responde con 'Si' o 'No'")
-    addresp = str(input("¿Hay otrx responsable del proyecto? Si/No  "))
-    addresp = addresp.lower()
+    resp_check = input("Sos el/la responsable de este proyecto? Si/No  ")
+    resp_check = resp_check.lower()
 
-while addresp == "si":
+if resp_check == "si":
+
+    data['projects'][project_name]['responsables'] = [user_name, ]
+
+elif resp_check == "no":
+
+    project_resp = input("Por favor ingresa el nombre del responsable  ")
+    data['projects'][project_name]['responsables'] = project_resp
+
+add_resp = str(input("¿Hay otrx responsable del proyecto? Si/No  "))
+add_resp = add_resp.lower()
+
+while add_resp != "si" and add_resp != "no":
+
+    print("Por favor responde con 'Si' o 'No'")
+    add_resp = str(input("¿Hay otrx responsable del proyecto? Si/No  "))
+    add_resp = add_resp.lower()
+
+while add_resp == "si":
 
     RespAdic = str(input("Ingresá el nombre de la persona responsable  "))
-    data['projects'][ProjectName]['responsables'].append(RespAdic)
-    addresp = str(input("¿Hay otrx responsable del proyecto? Si/No  "))
-    addresp = addresp.lower()
+    data['projects'][project_name]['responsables'].append(RespAdic)
+    add_resp = str(input("¿Hay otrx responsable del proyecto? Si/No  "))
+    add_resp = add_resp.lower()
 
-    while addresp != "si" and addresp != "no":
+    while add_resp != "si" and add_resp != "no":
 
         print("Por favor responde con 'Si' o 'No'")
-        addresp = str(input("¿Hay otrx responsable del proyecto? Si/No  "))
-        addresp = addresp.lower()
+        add_resp = str(input("¿Hay otrx responsable del proyecto? Si/No  "))
+        add_resp = add_resp.lower()
 
 print ("La información fue cargada correctamente")
 print ("Gracias!")
